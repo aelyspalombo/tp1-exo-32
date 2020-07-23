f3bc6f34107268bce6ed870dad9523a0fd332177
aelyspalombo <68046095+aelyspalombo@users.noreply.github.com>
2020-07-23 14:13:29 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,18 +4,12 @@ basic.forever(function () {
     x = randint(0, 4)
     y = 0
     led.plot(x, y)
-    while (y != 4) {
+    for (let index = 0; index < 5; index++) {
         basic.pause(100)
-        led.plotBrightness(x, y, 175)
+        led.plotBrightness(x, y, led.brightness() - 80)
         y += 1
-        led.unplot(x, y - 3)
-        led.plot(x, y)
-        led.plotBrightness(x, y - 2, 60)
     }
-    basic.pause(100)
-    led.unplot(x, y - 2)
-    basic.pause(100)
-    led.unplot(x, y - 1)
-    basic.pause(100)
-    led.unplot(x, y)
+})
+basic.forever(function () {
+	
 })
