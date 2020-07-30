ba5d09e213b2c4c6159762de89ca937dfcf103ef
aelyspalombo <68046095+aelyspalombo@users.noreply.github.com>
2020-07-30 14:24:20 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,23 @@
-let x = randint(0, 4)
-let y = 0
-led.plot(x, y)
+let ligne_inversée = 0
+let iterations_avant_nelle_comette = 0
 basic.forever(function () {
-    while (y < 7) {
-        basic.pause(100)
-        y += 1
-        led.plot(x, y)
-        led.plotBrightness(x, y - 1, 170)
-        led.plotBrightness(x, y - 2, 85)
-        led.unplot(x, y - 3)
-        if (y == 4) {
-            x = randint(0, 4)
-            led.plot(x, 0)
+    if (iterations_avant_nelle_comette == 0) {
+        led.plot(randint(0, 4), 0)
+        iterations_avant_nelle_comette = 2
+    }
+    iterations_avant_nelle_comette += -1
+    basic.pause(500)
+    for (let colonne = 0; colonne <= 4; colonne++) {
+        for (let ligne = 0; ligne <= 4; ligne++) {
+            ligne_inversée = 4 - ligne
+            if (led.point(colonne, ligne_inversée) && led.pointBrightness(colonne, ligne_inversée) == 255) {
+                led.plotBrightness(colonne, ligne_inversée, 170)
+                led.plotBrightness(colonne, ligne_inversée + 1, 255)
+            } else if (led.pointBrightness(colonne, ligne_inversée) == 170) {
+                led.plotBrightness(colonne, ligne_inversée, 85)
+            } else if (led.pointBrightness(colonne, ligne_inversée) == 85) {
+                led.plotBrightness(colonne, ligne_inversée, 0)
+            }
         }
     }
 })
